87bcfb251c834b90df9f88df11aa23d4d31dd24c
ajlopez <webmaster@ajlopez.com>
2019-03-17 16:45:09 -0300

    Parse two specific tokens

--- test/tokens.js
+++ test/tokens.js
@@ -43,6 +43,7 @@ exports['parse specific name'] = function (test) {
     
     pdef.define('integer', 'integer:', function (value) { return parseInt(value); });
     pdef.define('if', 'name:if', function () { return 'if'; });
+    pdef.define('for', 'name:for', function () { return 'for'; });
     pdef.define('name', 'name:', function (value) { return value; });
     
     const lexer = ldef.lexer('if');
@@ -59,5 +60,28 @@ exports['parse specific name'] = function (test) {
     const result2 = parser2.parse('if');
     
     test.equal(result2, null);
+    
+    const result3 = parser2.parse('for');
+    
+    test.ok(result3);
+    test.strictEqual(result3, 'for');
+}
+
+exports['parse two specific tokens'] = function (test) {
+    const pdef = gepars.definition();
+    
+    pdef.define('integer', 'integer:', function (value) { return parseInt(value); });
+    pdef.define('each', 'name:each', function () { return 'if'; });
+    pdef.define('for', 'name:for', function () { return 'for'; });
+    pdef.define('foreach', [ 'name:for', 'name:each' ], function () { return 'foreach'; });
+    pdef.define('name', 'name:', function (value) { return value; });
+    
+    const lexer = ldef.lexer('for each');
+    const parser = pdef.parser(lexer);
+    
+    const result = parser.parse('foreach');
+    
+    test.ok(result);
+    test.strictEqual(result, 'foreach');
 }
 
