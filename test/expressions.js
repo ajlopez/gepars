807438cae01c9085cf8fefedb3912a70aa1adaed
ajlopez <webmaster@ajlopez.com>
2019-03-17 17:21:54 -0300

    Parse first expression; using gelex@0.0.2

---
+++ test/expressions.js
@@ -0,0 +1,26 @@
+
+const gepars = require('..');
+const gelex = require('gelex');
+
+// define lexer
+const ldef = gelex.definition();
+
+ldef.define('integer', '[0-9][0-9]*');
+ldef.define('name', '[a-zA-Z][a-zA-Z]*');
+ldef.define('operator', '+-*/'.split(''));
+
+exports['parse add expression'] = function (test) {
+    const pdef = gepars.definition();
+    
+    pdef.define('integer', 'integer:', function (value) { return parseInt(value); });
+    pdef.define('expression', [ 'integer', 'operator:+', 'integer' ], function (values) { return values; });
+    
+    const lexer = ldef.lexer('42 + 1');
+    const parser = pdef.parser(lexer);
+    
+    const result = parser.parse('expression');
+    
+    test.ok(result);
+    test.deepEqual(result, [ 42, '+', 1 ]);
+};
+
