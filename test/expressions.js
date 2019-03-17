ab7534e68dbd178ee80cf10959d6e3831a74b2b9
ajlopez <webmaster@ajlopez.com>
2019-03-17 17:31:04 -0300

    Additional test, parse multiply expression

--- test/expressions.js
+++ test/expressions.js
@@ -24,3 +24,19 @@ exports['parse add expression'] = function (test) {
     test.deepEqual(result, [ 42, '+', 1 ]);
 };
 
+exports['parse multiply expression'] = function (test) {
+    const pdef = gepars.definition();
+    
+    pdef.define('integer', 'integer:', function (value) { return parseInt(value); });
+    pdef.define('expression', [ 'integer', 'operator:+', 'integer' ], function (values) { return values; });
+    pdef.define('expression', [ 'integer', 'operator:*', 'integer' ], function (values) { return values; });
+    
+    const lexer = ldef.lexer('21 * 2');
+    const parser = pdef.parser(lexer);
+    
+    const result = parser.parse('expression');
+    
+    test.ok(result);
+    test.deepEqual(result, [ 21, '*', 2 ]);
+};
+
