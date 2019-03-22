c360c5d9f8560ae6f21098f5a2b60640ae34e770
ajlopez <webmaster@ajlopez.com>
2019-03-22 12:51:56 -0300

    Interpreter sample, WIP

--- samples/interpreter/interpret.js
+++ samples/interpreter/interpret.js
@@ -8,10 +8,18 @@ const code = process.argv[2];
 const ldef = gelex.definition();
 ldef.define('integer', '[0-9][0-9]*');
 ldef.define('name', '[a-z][a-z]*');
-ldef.define('operator', '+-*/'.split(''));
-ldef.define('delimiter', '()'.split(''));
+ldef.define('operator', '+-*/='.split(''));
+ldef.define('delimiter', '();,'.split(''));
 
 const pdef = gepars.definition();
+
+pdef.define('program', 'commandlist', function (value) { return geast.sequence(value); });
+
+pdef.define('commandlist', [ 'commandlist', 'command' ], function (values) { values[0].push(values[1]); return values[0]; });
+pdef.define('commandlist', 'command', function (value) { return [ value ]; });
+
+pdef.define('command', [ 'name:let', 'name:', 'delimiter:;' ], function (values) { return geast.variable(values[1]); });
+pdef.define('command', [ 'name:', 'operator:=', 'expression', 'delimiter:;' ], function (values) { return geast.assignment(geast.name(values[0]), values[2]); });
 
 pdef.define('expression', 'expression0');
 pdef.define('expression0', [ 'expression0', 'operator:+', 'expression1' ], function (values) { return geast.binary(values[1], values[0], values[2]); });
@@ -20,16 +28,43 @@ pdef.define('expression0', 'expression1');
 pdef.define('expression1', [ 'expression1', 'operator:*', 'term' ], function (values) { return geast.binary(values[1], values[0], values[2]); });
 pdef.define('expression1', [ 'expression1', 'operator:/', 'term' ], function (values) { return geast.binary(values[1], values[0], values[2]); });
 pdef.define('expression1', 'term');
+
 pdef.define('term', 'integer:', function (value) { return geast.constant(parseInt(value)); });
+pdef.define('term', 'name:', function (value) { return geast.name(value); });
 pdef.define('term', [ 'delimiter:(', 'expression', 'delimiter:)' ], function (values) { return values[1]; });
 
-const lexer = ldef.lexer(code);
+const lexer = ldef.lexer('let a; a = 1;');
 const parser = pdef.parser(lexer);
 
-const expression = parser.parse('expression');
+const expression = parser.parse('program');
 
 function Interpreter() {
+    const context = {};
+    
+    this.processSequence = function (node) {
+        const nodes = node.nodes();
+        let result;
+        
+        for (let k = 0; k < nodes.length; k++)
+            result = nodes[k].process(this);
+        
+        return result;
+    };
+    
+    this.processVariable = function (node) {
+        context[node.name()] = null;
+    };
+    
+    this.processAssignment = function (node) {
+        const value = node.value().process(this);
+        
+        context[node.lefthand().name()] = value;
+        
+        return value;
+    ;}
+    
     this.processConstant = function (node) { return node.value(); };
+    
     this.processBinary = function (node) {
         const lvalue = node.left().process(this);
         const rvalue = node.right().process(this);
