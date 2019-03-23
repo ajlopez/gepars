c7bd0591cdcc9860efcd7f0eff36198898f3de33
ajlopez <webmaster@ajlopez.com>
2019-03-23 15:07:44 -0300

    New commands and expressions in interpreter sample

--- samples/interpreter/interpret.js
+++ samples/interpreter/interpret.js
@@ -12,6 +12,7 @@ const ldef = gelex.definition();
 ldef.define('integer', '[0-9][0-9]*');
 ldef.define('name', '[a-z][a-z]*');
 ldef.define('operator', '+-*/='.split(''));
+ldef.define('operator', '=== == <= < > =>'.split(' '));
 ldef.define('delimiter', '();,'.split(''));
 ldef.defineText('string', '"', '"');
 
@@ -22,17 +23,26 @@ pdef.define('program', 'commandlist', function (value) { return geast.sequence(v
 pdef.define('commandlist', [ 'commandlist', 'command' ], function (values) { values[0].push(values[1]); return values[0]; });
 pdef.define('commandlist', 'command', function (value) { return [ value ]; });
 
-pdef.define('command', [ 'name:print', 'expression', 'delimiter:;' ], function (values) { return geast.print(values[1]); });
-pdef.define('command', [ 'name:let', 'name:', 'delimiter:;' ], function (values) { return geast.variable(values[1]); });
-pdef.define('command', [ 'name:', 'operator:=', 'expression', 'delimiter:;' ], function (values) { return geast.assignment(geast.name(values[0]), values[2]); });
+pdef.define('command', [ 'simplecommand', 'delimiter:;' ], function (values) { return values[0]; });
+
+pdef.define('simplecommand', [ 'name:print', 'expression' ], function (values) { return geast.print(values[1]); });
+pdef.define('simplecommand', [ 'name:let', 'name:' ], function (values) { return geast.variable(values[1]); });
+pdef.define('simplecommand', [ 'name:', 'operator:=', 'expression' ], function (values) { return geast.assignment(geast.name(values[0]), values[2]); });
 
 pdef.define('expression', 'expression0');
-pdef.define('expression0', [ 'expression0', 'operator:+', 'expression1' ], function (values) { return geast.binary(values[1], values[0], values[2]); });
-pdef.define('expression0', [ 'expression0', 'operator:-', 'expression1' ], function (values) { return geast.binary(values[1], values[0], values[2]); });
+pdef.define('expression0', [ 'expression0', 'operator:==', 'expression2' ], function (values) { return geast.binary(values[1], values[0], values[2]); });
+pdef.define('expression0', [ 'expression0', 'operator:===', 'expression2' ], function (values) { return geast.binary(values[1], values[0], values[2]); });
+pdef.define('expression0', [ 'expression0', 'operator:<=', 'expression2' ], function (values) { return geast.binary(values[1], values[0], values[2]); });
+pdef.define('expression0', [ 'expression0', 'operator:<', 'expression2' ], function (values) { return geast.binary(values[1], values[0], values[2]); });
+pdef.define('expression0', [ 'expression0', 'operator:>=', 'expression2' ], function (values) { return geast.binary(values[1], values[0], values[2]); });
+pdef.define('expression0', [ 'expression0', 'operator:>', 'expression2' ], function (values) { return geast.binary(values[1], values[0], values[2]); });
 pdef.define('expression0', 'expression1');
-pdef.define('expression1', [ 'expression1', 'operator:*', 'term' ], function (values) { return geast.binary(values[1], values[0], values[2]); });
-pdef.define('expression1', [ 'expression1', 'operator:/', 'term' ], function (values) { return geast.binary(values[1], values[0], values[2]); });
-pdef.define('expression1', 'term');
+pdef.define('expression1', [ 'expression1', 'operator:+', 'expression2' ], function (values) { return geast.binary(values[1], values[0], values[2]); });
+pdef.define('expression1', [ 'expression1', 'operator:-', 'expression2' ], function (values) { return geast.binary(values[1], values[0], values[2]); });
+pdef.define('expression1', 'expression2');
+pdef.define('expression2', [ 'expression2', 'operator:*', 'term' ], function (values) { return geast.binary(values[1], values[0], values[2]); });
+pdef.define('expression2', [ 'expression2', 'operator:/', 'term' ], function (values) { return geast.binary(values[1], values[0], values[2]); });
+pdef.define('expression2', 'term');
 
 pdef.define('term', 'integer:', function (value) { return geast.constant(parseInt(value)); });
 pdef.define('term', 'string:', function (value) { return geast.constant(value); });
@@ -95,6 +105,18 @@ function Interpreter() {
             return lvalue * rvalue;
         if (node.operator() === '/')
             return lvalue / rvalue;
+        if (node.operator() === '===')
+            return lvalue === rvalue;
+        if (node.operator() === '==')
+            return lvalue == rvalue;
+        if (node.operator() === '<=')
+            return lvalue <= rvalue;
+        if (node.operator() === '<')
+            return lvalue < rvalue;
+        if (node.operator() === '>')
+            return lvalue > rvalue;
+        if (node.operator() === '>=')
+            return lvalue >= rvalue;
     }
 }
 
